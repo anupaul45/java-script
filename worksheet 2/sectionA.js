3162748a837c834eb4b4620bb0c9343056807eac
anupaul46 <paulanu6116@gmail.com>
2025-05-21 20:40:52 +0530

    added

---
+++ worksheet 2/sectionA.js
@@ -0,0 +1,14 @@
+//let str =" alphaintern "
+//console.log(str.trim().toUpperCase())
+
+//let str ="webdevelopment"
+//console.log(str.slice(3,6))
+
+//let str ="alphaintern"
+//console.log(str.substring(6))
+
+//let str =" welcome to alphaintern"
+//console.log(str.includes("alpha"))
+
+let char ="A"
+console.log(char.charCodeAt(0)) 
---
+++ worksheet 2/sectionB.js
@@ -0,0 +1,17 @@
+//let name = "Riya";
+//let score = 92;
+//let subject = "web development";
+
+//console.log(`Hello ${name}, you scored ${score} in ${subject}!`);
+
+let myName = "anu";
+console.log(myName);
+
+let str1 = "alpha";
+let str2 = "intern";
+
+let combined = `${str1} ${str2}`;
+console.log(combined);
+
+let str = "alphaintern";
+console.log(str.slice(3))
